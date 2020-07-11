9a04dac023fd962598c8df7048d4a210cbe4fa36
MarkSFrancis <mrkfrancis8@gmail.com>
2020-07-11 15:01:18 +0100

    Recreated react app from scratch - ditching create-react-app

--- apps/sample-react/src/index.tsx
+++ apps/sample-react/src/index.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Log } from "@yarn-monorepo/log";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+
+Log("Hello from React!");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App title="Hello React!" />,
+  document.getElementById("output")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
